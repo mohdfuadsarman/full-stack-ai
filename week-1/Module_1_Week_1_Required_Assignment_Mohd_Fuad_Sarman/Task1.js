813f9d9f5068a012d0055632dea3bd5aa653e5a7
Mohd Fuad <mohdfuadsarman@gmail.com>
2025-05-11 14:39:48 +0800

    update assignment

---
+++ week-1/Module_1_Week_1_Required_Assignment_Mohd_Fuad_Sarman/Task1.js
@@ -0,0 +1,13 @@
+// Task 1: Variables and Data Types
+
+// Declare variable x and assign it with value 10
+let x = 10;
+
+// Declare variable y and assign it with value "Hello"
+let y = "Hello";
+
+// Declare variable result and concatenate x and y
+let result = x + y;
+
+// Print the result
+console.log(result); // Output: 10Hello
